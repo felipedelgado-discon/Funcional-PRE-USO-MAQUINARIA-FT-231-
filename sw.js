9a049a7d43a32ffa3db47b77c1106376054b5f1b
felipedelgado-discon <felipe.delgado@discon.com.co>
2026-09-10 16:59:04 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
-const CACHE_NAME = 'ft231-v3';
+const CACHE_NAME = 'ft231-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Abriendo caché y guardando archivos');
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
@@ -19,7 +31,10 @@ self.addEventListener('fetch', (e) => {
     e.respondWith(fetch(e.request));
     return;
   }
+  // Para todo lo demás (html, manifest), busca en caché primero
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
